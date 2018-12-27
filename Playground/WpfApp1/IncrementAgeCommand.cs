85b6dc22e4ca81d91d8abc5600f6a9a61ad99379
sgwr164 <h.sugawara.1983@gmail.com>
2018-12-27 17:22:39 +0900

    wpfを試してみる

---
+++ Playground/WpfApp1/IncrementAgeCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class IncrementAgeCommand : ICommand
+    {
+        private Person vm;
+
+        public IncrementAgeCommand(Person viewmodel)
+        {
+            vm = viewmodel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return (vm.Age < 17);
+        }
+
+        public void Execute(object parameter)
+        {
+            vm.Age++;
+            CanExecuteChanged(parameter, null);
+        }
+    }
+}
---
+++ Playground/WpfApp1/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class Person: INotifyPropertyChanged
+    {
+        public ICommand IncrementAge { get; set; }
+
+        public Person()
+        {
+            IncrementAge = new IncrementAgeCommand(this);
+        }
+
+        private string firstNameVal;
+        public string FirstName
+        {
+            get { return firstNameVal; }
+            set
+            {
+                firstNameVal = value;
+                NotifyPropertyChanged("FirstName");
+            }
+        }
+
+        private string lastNameVal;
+        public string LastName
+        {
+            get { return lastNameVal; }
+            set
+            {
+                lastNameVal = value;
+                NotifyPropertyChanged("LastName");
+            }
+        }
+
+        private int ageVal;
+        public int Age
+        {
+            get { return ageVal; }
+            set
+            {
+                ageVal = value;
+                NotifyPropertyChanged("Age");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
