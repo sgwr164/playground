dc84276debe78c27b77f17ec57b8a7b94995a4e9
sgwr164 <h.sugawara.1983@gmail.com>
2019-03-14 17:33:27 +0900

    Add Calc

---
+++ Playground/fspg/Calc.fs
@@ -0,0 +1,6 @@
+﻿module Calc
+
+let rec factorial = function
+    | 1 -> 1
+    | n when n <= 0 -> 0
+    | n -> n * factorial (n - 1)
---
+++ Playground/fspg/CalcTest.fs
@@ -0,0 +1,12 @@
+﻿module CalcTest
+
+open NUnit.Framework
+
+[<TestCase(0,0)>]
+[<TestCase(1,1)>]
+[<TestCase(2,2)>]
+[<TestCase(3,6)>]
+[<TestCase(4,24)>]
+[<TestCase(5,120)>]
+let factorialTest(value: int, expected: int) :unit =
+    Assert.AreEqual(Calc.factorial value, expected)
