87d58a4cc7af96965d5774ca1a8992338888d728
sgwr164 <h.sugawara.1983@gmail.com>
2019-03-14 17:11:59 +0900

    Add SampleTest.cs

---
+++ Playground/player/SampleTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace player
+{
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.AreEqual(1 * 2, 2);
+        }
+    }
+}
