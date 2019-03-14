afc16bc92ddadda5acfe87cbe2b25ec37086f487
sgwr164 <h.sugawara.1983@gmail.com>
2019-03-14 16:40:51 +0900

    Add NUnit

---
+++ Playground/fspg/SampleTest.fs
@@ -0,0 +1,15 @@
+﻿module SampleTest
+
+open NUnit.Framework
+ 
+[<Test>]
+let simpleTest() =
+    Assert.AreEqual(2.0**10.0, 1024.0)
+ 
+[<TestCase("",      0)>]
+[<TestCase("ab",    2)>]
+[<TestCase("ABC",   3)>]
+[<TestCase("ABCD",  4)>]
+[<TestCase("12345", 5)>]
+let parameterizedTest(str: string, expected: int) :unit =
+    Assert.AreEqual(str.Length, expected)
